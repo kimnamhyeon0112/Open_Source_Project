7e2c177ccd6a1f848b6941aeeec4e4b73621bdf9
Yong In <brin7584@gmail.com>
2022-04-30 08:24:31 +0900

    feat : 검색어 입력 전 추천명령어 숨김 검색창에 검색어를 입력하기 전에는 추천 검색어들을 숨기는 기능 추가

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-const search= ()=>{
-	const searchbox = document.getElementById("Search_box").value.toUpperCase();
-	const command_list = document.getElementById("command_list")
-	const command = document.querySelectorAll(".command")
-	const commands = command_list.getElementsByTagName("li")
+const inputBox = document.querySelector("#Search_box");
+const ta = document.querySelector("#command_list");
 
-	for(var i=0; i<commands.length;i++){
+inputBox.addEventListener("keyup", (e)=>{
+	if(e.target.value.length >0){
+		ta.classList.remove('invisible');
+		// searchbox.style.setProperty('border-radius', '0px');
+	}
+	else{
+		ta.classList.add('invisible');
+		// searchbox.style.setProperty('border-radius', '15px');
+	}
+})
+
+
+const search_Recommends= ()=>{
+	const searchbox = document.getElementById("Search_box");
+	let command_list = document.getElementById("command_list");
+	let command = document.querySelectorAll(".command");
+	let commands_tags = command_list.getElementsByTagName("li");
+
+	for(var i=0; i<commands_tags.length;i++){
 		let match = command[i];
 
 		if(match){
 			let textvalue=match.textContent || match.innerHTML
 			
-			if(textvalue.toUpperCase().indexOf(searchbox)>-1){
+			if(textvalue.toUpperCase().indexOf(searchbox.value.toUpperCase())>-1){
 				command[i].style.display = "";
 			}else {
 				command[i].style.display = "none";
 			}
 		}
-	}
+	}	
 }
