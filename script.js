5c9ea57fdca61e39702cb707b97ce71deeb81ce6
Yong In <brin7584@gmail.com>
2022-05-09 22:22:47 +0900

    feat : 검색창과 command 비교 검색창에서 넣은 값과 준비된 command 비교해서 맞으면 다른 html로 이동

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+let searchbox = document.getElementById("Search_box");
+let command_list = document.getElementById("command_list");
+let command = document.getElementsByClassName("command");
 const search_Recommends= ()=>{
-	let searchbox = document.getElementById("Search_box");
-	let command_list = document.getElementById("command_list");
-	let command = document.getElementsByClassName("command");
 	
 	//검색창에 문자가 입력되면, 추천 검색어를 보이게 수정 && 검색창 모서리를 뾰족하게 만듦
 	if(searchbox.value.length >0){
@@ -30,11 +30,26 @@ const search_Recommends= ()=>{
 }
 
 //검색창에서 Enter 입력하면 해당 검색값이 ul에 있으면 WorldMap.html로 이동시키는 것
-var input = document.getElementById("Search_box");
-input.addEventListener("keypress", function(event) {
+searchbox.addEventListener("keypress", function(event) {
+
 	// If the user presses the "Enter" key on the keyboard
 	if (event.key === "Enter") {
-		// Enter 눌리면, 해당 html창으로 이동
-		window.location.href='WorldMap.html';
+		for(var i=0; i<(command_list.getElementsByTagName("li")).length;i++){
+			let match = command[i];
+			
+			if(match){
+				let textvalue=match.textContent || match.innerHTML
+				
+				if(textvalue.toUpperCase()==searchbox.value.toUpperCase()){
+					window.location.href='WorldMap.html';
+					break;
+				}else {
+					alert("Not valid");
+					break;
+				}
+			}
+		}	
+
 	}
+	
 });
