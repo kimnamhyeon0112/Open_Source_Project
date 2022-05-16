96416d04cc547961564f4d2b4a2e4383e1a028e9
huntedu <best00517@naver.com>
2022-05-17 01:04:07 +0900

    style : 마우스 올릴 시 국가명 출력 마우스 커서 위치를 이용해 해당 위치에 있을 경우 툴팁창을 띄워 구현하는 방법을 선택하였으나, info-tooltip id 태그에 맞는 data값을 주는 방법을 구현하지 못하였습니다. 마우스 위치에 따라 path가 지정되도록 구현해야 됩니다.

--- world_map/WorldMap.js
+++ world_map/WorldMap.js
@@ -13,3 +13,29 @@ dfd.readCSV(remote_url)
 .then(function(data){
     data.plot ("plot_div").pie({ config: { values: "Quantity", labels: "Country" } });
  });
+
+    // 브라우서 호환 ( 크로스브라우징 ) 체크 하여 문서 전체에 mousemove 이벤트를 걸어줍니다.
+    if ( document.addEventListener ) {
+        document.addEventListener("mousemove",resultFun,false);
+    } else if ( document.attachEvent ) {
+        document.attachEvent("onmousemove",resultFun);
+    } else {
+        document.onmousemove = resultFun;
+    }
+    
+    //  문서에서 마우스가 움직일때(mousemove) 마다 resultFun() 함수가 실행됩니다.
+    //  x.clientX,Y 는 페이지에서 이벤트 발생한 X,Y 좌표를 가져옵니다.
+    function resultFun(x) {
+        var xY = x.clientX +  " * "  + x.clientY ;
+        var positionLeft = x.clientX;
+        var positionTop = x.clientY;
+        // result  div 에 innerHTML 로 xY 변수를 적용 합니다.
+       
+        document.getElementById('info-tooltip').innerHTML = 'china'
+        //  result  div 의 position 위치를  positionLeft, Top 변수 값으로 style 적용해줍니다.
+        document.getElementById('info-tooltip').style.display =  'block' ;
+        document.getElementById('info-tooltip').style.left = ( positionLeft - 25 ) + "px";
+        document.getElementById('info-tooltip').style.top = ( positionTop + 30 ) + "px";
+    }
+
+
