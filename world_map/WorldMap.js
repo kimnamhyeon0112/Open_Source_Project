b9e196199ba97ddd2abd50ede128bdc4dbfb77e6
Yong In <brin7584@gmail.com>
2022-05-20 17:34:19 +0900

    feat : Nasdaq API 호출 Nasdaq API의 OPEC Crude Oil Price data를 불러와 날짜와 가격으로 분류해 데이터를 저장. API_KEY 변수에 Nasdaq에서 제공하는 API_KEY값을 넣으면 된다. 해당 값은 Nasdaq 웹사이트에서 회원가입을 하면 얻을 수 있다.

--- world_map/WorldMap.js
+++ world_map/WorldMap.js
@@ -8,3 +8,29 @@ if(localStorage.getItem('inputValue')){
     CommodityName.textContent = textvalue.substring(0,(textvalue.indexOf(' ')));
 }
 
+
+API_KEY = 'NULL';
+let stockChartXvalues = [];
+let stockChartYvalues = [];
+
+let stockChartXvaluesFunction=[];
+let stockChartYvaluesFunction=[];
+
+
+fetch(`https://data.nasdaq.com/api/v3/datasets/OPEC/ORB?start_date=2022-01-01&end_date=2022-05-18&api_key=${API_KEY}`)
+.then((response) => response.json())
+.then(
+    function(data){
+        console.log(data.dataset);
+
+        for (var key in (data.dataset)[`data`]){
+          
+            stockChartXvaluesFunction.push(data.dataset.data[key][`0`]);
+            stockChartYvaluesFunction.push(data.dataset.data[key][`1`]);
+        }
+
+    }
+)
+
+
+
