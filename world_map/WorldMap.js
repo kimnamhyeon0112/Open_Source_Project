e29ce9fdc6b5ab9352ba491d720c2b9a76b835e4
Yong In <brin7584@gmail.com>
2022-05-23 21:26:33 +0900

    feat : chart 생성 일반화 차트 만드는 방식을 함수를 통해 이루어지도록 하여 API를 통해 얻는 데이터 종류와 상관없이 차트를 여러 개 만들 수 있다.

--- world_map/WorldMap.js
+++ world_map/WorldMap.js
@@ -1,40 +1,69 @@
 //localstroage에서 사용자가 입력한 text를 통해 html Tag의 content 기입
-if(localStorage.getItem('inputValue')){
-    const textvalue = localStorage.getItem('inputValue')
-    let h1TagContent = document.getElementById("Header")    
-    let CommodityName = document.getElementById("commodity_name");
-
-    h1TagContent.textContent = textvalue
-    CommodityName.textContent = textvalue.substring(0,(textvalue.indexOf(' ')));
+// if(localStorage.getItem('inputValue')){
+  const textvalue = localStorage.getItem('inputValue')
+  let h1TagContent = document.getElementById("Header")    
+  let CommodityName = document.getElementById("commodity_name");
+  
+  h1TagContent.textContent = textvalue
+  CommodityName.textContent = textvalue.substring(0,(textvalue.indexOf(' ')));
+  // }
+  
+let chartsheet = {
+  type: 'line',
+  options: {
+    title: {
+      display: true,
+      text: CommodityName.textContent
+    }
+  }
 }
-// let API_Code = {
-//   CrudeOil : [`OPEC/ORB`,`EUREX/FCPEM2024.json`],
-//   NaturalGas : ["BSE/BOM519003.json"],
-//   Coal : ["HKEX/03948.json", "HKEX/01898.json", "HKEX/00835.json", "BSE/BOM533278.json"],
+let PriceChart = new Chart(document.getElementById("Price_LineChart"), chartsheet);
 
-//   Aluminium : "SHFE/ALQ2022.json",
-//   Copper : ["HKEX/00358.json", "SHFE/CUQ2022.json"] ,
-//   Zinc : "SHFE/ZNQ2022.json",
-//   Nickel : "SHFE/NIN2022.json",
-//   Lead : "SHFE/PBQ2022.json",
-//   Tin : "",
-//   IronOre : "HKEX/01053.json",
-//   Gold :"SHFE/AUX2021" ,
-//   Silver : ["SHFE/AGQ2022",	"LBMA/SILVER"],
-//   Cobalt : "ODA/PCOBA_USD.json",
-//   Lithium : "",
+function addData( ChartYvalues, _label){
+  //데이터셋 추가
+  let newDataset = {
+			label: _label,
+			borderColor: getRandomColor(),
+			data: ChartYvalues,
+			fill: false
+  }
+  // chart에 newDataset 푸쉬
+  PriceChart.data.datasets.push(newDataset);
 
-//   BaseMetals : "ODA/PMETA_INDEX.json",
+  PriceChart.update();	//차트 업데이트
+}
 
-//   Corn : "",
-//   Wheat : "",
-//   Rice : "",
-//   Oats : "",
-//   SoybeanOil : "",
-//   Soybean : "",
-//   Sugar : "",
-//   Coffee : ""
-// }
+const API_KEY = 'NULL';
+let series = "NULL";
+
+  let API_Code = {
+  CrudeOil : [`OPEC/ORB`,`EUREX/FCPEM2024.json`],
+  NaturalGas : {url : `https://api.eia.gov/v2/natural-gas/pri/fut/data/?api_key=${API_KEY}&frequency=daily&start=2021-01-01&sort[0][column]=period&sort[0][direction]=asc&data[]=value&facets[series][]=${series}`,series : [`RNGWHHD`,"RNGC1","RNGC2", "RNGC3", "RNGC4"] },
+  Coal : ["HKEX/03948.json", "HKEX/01898.json", "HKEX/00835.json", "BSE/BOM533278.json"],
+
+  Aluminium : "SHFE/ALQ2022.json",
+  Copper : ["HKEX/00358.json", "SHFE/CUQ2022.json"] ,
+  Zinc : "SHFE/ZNQ2022.json",
+  Nickel : "SHFE/NIN2022.json",
+  Lead : "SHFE/PBQ2022.json",
+  Tin : "",
+  IronOre : "HKEX/01053.json",
+  Gold :"SHFE/AUX2021" ,
+  Silver : ["SHFE/AGQ2022",	"LBMA/SILVER"],
+  Cobalt : "ODA/PCOBA_USD.json",
+  Lithium : "",
+
+  BaseMetals : "ODA/PMETA_INDEX.json",
+
+  Corn : "",
+  Wheat : "",
+  Rice : "",
+  Oats : "",
+  SoybeanOil : "",
+  Soybean : "",
+  Sugar : "",
+  Coffee : ""
+}
 
 function getToday(){
   let date = new Date();
@@ -57,7 +86,6 @@ function getStartdate(){
   return `${year}-${month}-${day}`;
 }
 
-const API_KEY = 'NULL';
 
 // let database_code = `OPEC/ORB`;
 
@@ -66,51 +94,60 @@ let start_date = getStartdate();
 
 let ChartXvalues = [];
 let ChartYvalues = [];
+series = API_Code[CommodityName.textContent][`series`];
+console.log(series);
 
-let series = `RNGC4`;
+let A=[];
+let B=[];
 
 // fetch(`https://data.nasdaq.com/api/v3/datasets/${database_code}?start_date=${start_date}&end_date=${end_date}&api_key=${API_KEY}`)
 
-fetch(`https://api.eia.gov/v2/natural-gas/pri/fut/data/?api_key=${API_KEY}&frequency=daily&start=2021-01-01&sort[0][column]=period&sort[0][direction]=desc&data[]=value&facets[series][]=${series}`)
+fetch(`https://api.eia.gov/v2/natural-gas/pri/fut/data/?api_key=${API_KEY}&frequency=daily&start=2021-01-01&sort[0][column]=period&sort[0][direction]=asc&data[]=value&facets[series][]=RNGC3`)
 .then((response) => response.json())
 .then(
     function(data){
-        // console.log(data);
+        console.log(data);
 
      
         for (var key in (data.response)[`data`]){
           
-            ChartXvalues.unshift((data.response.data)[key][`period`]);
-            ChartYvalues.unshift((data.response.data)[key][`value`]);
+            ChartXvalues.push((data.response.data)[key][`period`]);
+            ChartYvalues.push((data.response.data)[key][`value`]);
+            A.push(((data.response.data)[key][`period`]));
+            B.push((data.response.data)[key][`value`]+1);
         }
-        // console.log(ChartYvalues);
-        makeChart(ChartXvalues, ChartYvalues,series);
+        PriceChart.data.labels = ChartXvalues;
+        PriceChart.update();
+        addData(ChartYvalues,series);
+        addData(B, "TEST");
       }
 )
 function getRandomColor() {
 	return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-function makeChart(ChartXvalues, ChartYvalues, _label){
-    new Chart(document.getElementById("Price_LineChart"), {
-      type: 'line',
-      data: {
-        labels: ChartXvalues,
-        datasets: [{ 
-            data:  ChartYvalues,
-            label: _label,
-            borderColor: "#3e95cd",
-            fill: false
-          },
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'NaturalGas'
-        }
-      }
-    });
-}
+
+
+// function makeChart(ChartXvalues, ChartYvalues, _label){
+//   new Chart(document.getElementById("Price_LineChart"), {
+//       type: 'line',
+//       data: {
+//         labels: ChartXvalues,
+//         datasets: [{ 
+//             data:  ChartYvalues,
+//             label: _label,
+//             borderColor: getRandomColor(),
+//             fill: false
+//           },
+//         ]
+//       },
+//       options: {
+//         title: {
+//           display: true,
+//           text: 'NaturalGas'
+//         }
+//       }
+//     });
+// }
 
 
