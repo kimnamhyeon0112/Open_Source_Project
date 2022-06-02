3aa5d281024a86b5d67b96ca85af1673c25f4b26
Yong In <brin7584@gmail.com>
2022-06-02 13:41:33 +0900

    PR 준비

--- world_map/WorldMap.js
+++ world_map/WorldMap.js
@@ -1,18 +1,10 @@
 //localstroage에서 사용자가 입력한 text를 통해 html Tag의 content 기입
-if(localStorage.getItem('inputValue')){
-    const textvalue = localStorage.getItem('inputValue')
-    let h1TagContent = document.getElementById("Header")    
-    let CommodityName = document.getElementById("commodity_name");
-
-    h1TagContent.textContent = textvalue
-    CommodityName.textContent = textvalue.substring(0,(textvalue.indexOf(' ')));
-}
 const textvalue = localStorage.getItem('inputValue')
-var remote_url = 'https://raw.githubusercontent.com/kimnamhyeon0112/Open_Source_Project/Feature_PieData/DB/'+ textvalue +'.csv';
-dfd.readCSV(remote_url)
-.then(function(data){
-    data.plot ("plot_div").pie({ config: { values: "Quantity", labels: "Country" } });
- });
+let h1TagContent = document.getElementById("Header")    
+let CommodityName = document.getElementById("commodity_name");
+
+h1TagContent.textContent = textvalue
+CommodityName.textContent = textvalue.substring(0,(textvalue.indexOf(' ')));
 
     // 브라우서 호환 ( 크로스브라우징 ) 체크 하여 문서 전체에 mousemove 이벤트를 걸어줍니다.
     if ( document.addEventListener ) {
